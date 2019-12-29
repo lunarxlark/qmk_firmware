ff193dab460d75d1f791d05f5869c94096a258c6
lunarxlark <lunarxlark@gmail.com>
2019-12-30 02:30:48 +0900

    comment out unnecessary logic

--- keyboards/crkbd/keymaps/lunarxlark/keymap.c
+++ keyboards/crkbd/keymaps/lunarxlark/keymap.c
@@ -137,11 +137,11 @@ void matrix_init_user(void) {
 #ifdef SSD1306OLED
 
 // When add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
+//const char *read_layer_state(void);
 const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
+//void set_keylog(uint16_t keycode, keyrecord_t *record);
+//const char *read_keylog(void);
+//const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
@@ -155,12 +155,6 @@ void matrix_scan_user(void) {
 void matrix_render_user(struct CharacterMatrix *matrix) {
   if (is_master) {
     // If you want to change the display of OLED, you need to change here
-    //matrix_write_ln(matrix, read_layer_state());
-    //matrix_write_ln(matrix, read_keylog());
-    //matrix_write_ln(matrix, read_keylogs());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    //matrix_write_ln(matrix, read_timelog());
     matrix_write(matrix, read_logo());
   } else {
     matrix_write(matrix, read_logo());
@@ -183,12 +177,6 @@ void iota_gfx_task_user(void) {
 #endif//SSD1306OLED
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef SSD1306OLED
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
 
   switch (keycode) {
     case QWERTY:
